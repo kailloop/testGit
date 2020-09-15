c2aa2548a09a5adf3f6afa85f3f55c198e7ab274
master <82108@DESKTOP-SDBJJB4>
2020-09-15 11:15:59 +0900

    04_person에 주소, 키 추가 -bs-

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -4,6 +4,8 @@ public class Person {
 	//회원정보를 저장하는클래스
 	private String name;
 	private int age;
+	private String address;
+	private double height;
 	
 	public Person() {
 		// TODO Auto-generated constructor stub
@@ -23,6 +25,22 @@ public class Person {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
 	}
 	
 	
