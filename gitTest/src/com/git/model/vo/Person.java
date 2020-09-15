a66b3eb627c88c3c990ddea5b645eaa588e7eeb7
82108 <82108@DESKTOP-SDBJJB4>
2020-09-15 11:08:49 +0900

    01_person add properties name, age - je

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -2,6 +2,31 @@ package com.git.model.vo;
 
 public class Person {
 	//회원정보를 저장하는클래스
+	private String name;
+	private int age;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
 	
 	
 
